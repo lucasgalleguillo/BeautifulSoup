944deaa8a179cc4f2976939875195061712dd813
octalucero <octaviolucero2005@gmail.com>
2023-05-19 12:08:59 -0300

    Primero commit de ws

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-//aca se hace el web scraping
+from bs4 import BeautifulSoup
+import requests
+
+r = requests.get('https://listado.mercadolibre.com.ar/parlante-jbl')
+
+r.status_code
+
+soup = BeautifulSoup(r.content, 'html.parser')
+
+titulos = soup.find_all('h2', attrs={"class":'ui-search-item__title shops__item-title'})
+titulos = [i.text for i in titulos]
+
+urls = soup.find_all ('a', attrs={"class":'ui-search-item__group__element shops__items-group-details ui-search-link'})
+urls = [i.get('href') for i in urls]
+
