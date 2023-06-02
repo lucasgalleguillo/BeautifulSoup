5acf4e2dce613761df7af6722fe013cf5812add6
FacundoBisio <facubisio433@gmail.com>
2023-06-02 01:40:07 -0300

    Noticias en cuadros part1

--- ui.py
+++ ui.py
@@ -24,12 +24,12 @@ def scrape_website(url):
         # If the request was unsuccessful, return None
         return None
 
-var = pag_categoria("infobae", "economia")
-for i in range(len(var)):
-    for j in range(len(var[i])):
-       print(var[i][j], end=' ')
+cba = pag_categoria("cordoba","a")
 
-
+argi = pag_categoria("infobae", "inicio")
+arge = pag_categoria("infobae", "economia")
+argd = pag_categoria("infobae", "deportes")
+args = pag_categoria("infobae", "sociedad")
 
 # Streamlit app
 def main():
@@ -50,8 +50,6 @@ def main():
     pages = ["Home", "Córdoba", "Argentina", "Global"]
     selected_page = st.sidebar.selectbox("Select a page", pages)
     
-    
-
     if selected_page == "Home":
         st.header("¡Well come, we are Newsline!")
         st.markdown("We have all the news you want, from **wherever** you want and **whenever** you want")
@@ -61,20 +59,35 @@ def main():
         st.header("Córdoba news")
         st.markdown("Here we can find the latest news from **Córdoba**")
         
-
         datos = [
         {
-            'titulo': 's',
-            'texto': 'sss',
-            'imagen': 'https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg',
+            'titulo': cba[1][0],
+            'texto': cba[2][0],
+            'imagen': cba[0][0],
         },
         {
-            'titulo': 'ANTONELISMO',
-            'texto': 'Texto 2',
-            'imagen': 'https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg'
+            'titulo': cba[1][1],
+            'texto': cba[2][1],
+            'imagen': cba[0][1],
+        },    
+        {
+            'titulo': cba[1][2],
+            'texto': cba[2][2],
+            'imagen': cba[0][2],
         },
-        # Agrega más elementos si es necesario
+        {
+            'titulo': cba[1][3],
+            'texto': cba[2][3],
+            'imagen': cba[0][3],
+        },   
+        {
+            'titulo': cba[1][4],
+            'texto': cba[2][4],
+            'imagen': cba[0][4],
+        },   
         ]
+
+        
 
         # Crear un DataFrame de Pandas con los datos
         df = pd.DataFrame(datos)
@@ -83,7 +96,7 @@ def main():
             col1, col2 = st.columns([2, 1])
             col1.title(dato['titulo'])
             col1.write(dato['texto'])
-            col2.image(dato['imagen'], width=200)
+            col2.image(dato['imagen'], width=400)
 
     elif selected_page == "Argentina":
         col1, col2 = st.columns(2)
@@ -92,23 +105,61 @@ def main():
             st.header("Argentine news")
             st.markdown("Here we can find the latest news from **Argentina**")
 
-            st.subheader(var[0][2])
-            st.write(var[2][1])
-            
+        datos = [
+        {
+            'titulo': argi[0][0],
+            'texto': argi[2][1],
+            'imagen': argi[1][0],
+        },
+        {
+            'titulo': argi[0][1],
+            'texto': argi[2][2],
+            'imagen': argi[1][1],
+        },
+        {
+            'titulo': argi[0][2],
+            'texto': argi[2][0],
+            'imagen': argi[1][2],
+        },
 
-        with col2:
-            option = st.selectbox(
-                "How would you like to read?",
-                ("All", "Sport", "Economy", "Society"),
-            )
-            if option == "All":
-                st.image(var[0][0])
+        {
+            'titulo': arge[0][0],
+            'texto': arge[2][1],
+            'imagen': arge[1][0],
+        },
+        {
+            'titulo': arge[0][1],
+            'texto': arge[2][2],
+            'imagen': arge[1][1],
+        },
+        {
+            'titulo': arge[0][2],
+            'texto': arge[2][0],
+            'imagen': arge[1][2],
+        },        
+        ]
 
-            elif option == "Sport":
-                st.image(var[2][0])
+        df = pd.DataFrame(datos)
 
-            elif option == "Economy":
-                st.image(var[1][0])
+        for i, dato in df.iterrows():
+            col1, col2 = st.columns([2, 1])
+            col1.title(dato['titulo'])
+            col1.write(dato['texto'])
+            col2.image(dato['imagen'], width=400)    
+
+        """        with col2:
+                    option = st.selectbox(
+                        "How would you like to read?",
+                        ("All", "Sport", "Economy", "Society"),
+                    )
+                    if option == "All":
+                        st.image(cba[0][0])
+
+                    elif option == "Sport":
+                        st.image(cba[2][0])
+
+                    elif option == "Economy":
+                        st.image(cba[1][0])"""
 
         
     
@@ -128,3 +179,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
