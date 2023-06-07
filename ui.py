6c6618940c7310b56ddbf8e3933fa15336d0b103
joacohlavach <joaquin.hlavach@gmail.com>
2023-06-07 17:42:21 -0300

    for y categorias

--- ui.py
+++ ui.py
@@ -6,17 +6,6 @@ from funciones import *
 
 st.set_page_config(page_title = "Newsline", page_icon = ":newspaper:", layout = "wide")
 
-cba = pag_categoria("cordoba","a")
-argi = pag_categoria("infobae", "inicio")
-arge = pag_categoria("infobae", "economia")
-argd = pag_categoria("infobae", "deportes")
-args = pag_categoria("infobae", "sociedad")
-gli = pag_categoria("bbc", "inicio")
-gle = pag_categoria("bbc", "economia")
-glc = pag_categoria("bbc", "ciencia")
-glcu = pag_categoria("bbc", "cultura")
-
-
 def categoria(categoria, num):
     datos = []
     for i in range(num):
@@ -50,43 +39,82 @@ def main():
     )
 
     st.sidebar.title("Menu")
-    pages = ["Home", "Córdoba", "Argentina", "Global"]
+    pages = ["Inicio", "Córdoba", "Argentina", "Global"]
     selected_page = st.sidebar.selectbox("Seleciona una página", pages)
     
-    if selected_page == "Home":
+    if selected_page == "Inicio":
         st.header("¡Bienvenido, esto es Newsline!")
         st.markdown("### Las noticias ***que quieras***, de ***donde quieras*** y ***cuando quieras***")
+        st.subheader("¿Qué es Newsline?")
+        st.write("A más de uno alguna vez le incomodó tener que cambiar de página para poder ver las noticias de todas partes del mundo, ya que resulta tedioso. Por lo tanto Newsline busca ofrecernos todos estos servicios en UNA SOLA PÁGINA, y lo mejor de todo es que podrás encontrar noticias desde Córdoba al mundo entero.\n")
         st.markdown("Seguinos en Instagram, Facebook, Twitter, como: 'Newsline_on'")
 
     elif selected_page == "Córdoba":
+        cba = pag_categoria("cordoba","a")
+
         st.header("Noticias de Córdoba")
         st.markdown("Podrás encontrar las últimas noticias de **Córdoba**")
         
         categoria(cba, 7)
 
     elif selected_page == "Argentina":
+        argi = pag_categoria("infobae", "inicio")
+        arge = pag_categoria("infobae", "economia")
+        argd = pag_categoria("infobae", "deportes")
+        args = pag_categoria("infobae", "sociedad")
+
         col1, col2 = st.columns(2)
 
         with col1:
             st.header("Noticias de Argentina")
             st.markdown("Podrás encontrar las últimas noticias de **Argentina**")
 
-        categoria(argi, 3)    
-        categoria(arge, 3)
-        categoria(argd, 3)
-        categoria(args, 3)
+        sub_categ = st.selectbox("Selecione una opción", ["Inicio", "Economía", "Deportes", "Sociedad"])
+        if sub_categ == "Inicio":
+            st.write("Te encuentras en el Inicio")
+            categoria(argi, 3) 
+        
+        elif sub_categ == "Economía":
+            st.write("Selecionaste las noticias de Economía")
+            categoria(arge, 3)
+        
+        elif sub_categ == "Deportes":
+            st.write("Selecionaste las noticias de Deportes")
+            categoria(argd, 3)
+        
+        elif sub_categ == "Sociedad":
+            st.write("Selecionaste las noticias de Sociedad")
+            categoria(args, 3)
 
     elif selected_page == "Global":
+        gli = pag_categoria("bbc", "inicio")
+        gle = pag_categoria("bbc", "economia")
+        glc = pag_categoria("bbc", "ciencia")
+        glcu = pag_categoria("bbc", "cultura")
+
         col1, col2 = st.columns(2)
 
         with col1:
             st.header("Noticias Globales")
             st.markdown("Podrás encontrar las últimas noticias del **Mundo**")
-
+        
+        sub_categ = st.selectbox("Selecione una opción", ["Inicio", "Economía", "Ciencia", "Cultura"])
+        if sub_categ == "Inicio":
+            st.write("Te encuentras en el Inicio")
             categoria(gli, 3)
+        
+        elif sub_categ == "Economía":
+            st.write("Selecionaste las noticias de Economía")
             categoria(gle, 3)
+        
+        elif sub_categ == "Ciencia":
+            st.write("Selecionaste las noticias de Ciencia")
             categoria(glc, 3)
+        
+        elif sub_categ == "Cultura":
+            st.write("Selecionaste las noticias de Cultura")
             categoria(glcu, 3)
+
 
 if __name__ == '__main__':
     main()
